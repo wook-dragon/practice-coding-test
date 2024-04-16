7c106eeda38b945bf03b51d09c621a82e3b0c75d
wook <leeyong1321@gmail.com>
2024-04-16 21:23:09 +0900

    add: 배열제어하기

---
+++ src/배열제어하기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 배열제어하기 {
+	public static void main(String[] args) {
+		int[] arr = {4, 2, 2, 1, 3, 4};
+		System.out.println(Arrays.toString(solution(arr)));
+	}
+
+	private static int[] solution(int[] arr) {
+		Integer[] result = Arrays.stream(arr).boxed().distinct().toArray(Integer[]::new);
+		Arrays.sort(result, Collections.reverseOrder());
+
+		return Arrays.stream(result).mapToInt(Integer::intValue).toArray();
+	}
+}
